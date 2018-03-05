7c20fb178742c842ffbb70505229fb98d2293eff
Nermin H <nermingk@live.com>
2018-03-05 10:58:47 +0100

    clean core

--- app/code/local/Prijava/Model/Person/Collection.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Prijava_Model_Person_Collection extends Core_Model_Collection_Abstract
-{
-    protected $_itemObjectClass = 'Prijava_Model_Person';
-    protected $_table           = 'person';
-
-}
--- app/code/local/Prijava/View/Success.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Prijava_View_Success extends Core_View_Default
-{
-    public function _init()
-    {
-        $this->setTemplate(App::getConfig('Prijava','template/success'));
-        $this->setTitle("Prijava");
-    }
-}
